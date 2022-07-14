bd9f0f366ed5e71bb8cb0bff59b71c3aa74458b3
VincentGichohi <vincentegichohi@gmail.com>
2022-07-14 12:44:57 +0300

    Add allowed status to the models

--- scholarship_management/users/models.py
+++ scholarship_management/users/models.py
@@ -1,2 +1,9 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from users.utils.manage import BaseUserManager
+
+ALLOWED_STATUS = [
+    ("ACTIVE", "ACTIVE"),
+    ("SUSPENDED", "SUSPENDED")
+]
