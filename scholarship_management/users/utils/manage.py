2c7d84c3f79e02678b083beaa7aee8279b98606c
VincentGichohi <vincentegichohi@gmail.com>
2022-07-14 12:41:41 +0300

    Add a staff user

--- scholarship_management/users/utils/manage.py
+++ scholarship_management/users/utils/manage.py
@@ -2,31 +2,49 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, is_staff, password=None):
+    def create_user(self, username, email, is_staff, password=None):
         """
         Creates a user with a given email and password
         """
         if not username:
             raise ValueError("Enter a valid username")
+        if not email:
+            raise ValueError("Users must have an email address")
         user = self.model(
             username=username,
+            email=email,
             is_staff=True
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_staffuser(self, email, password):
+    def create_staffuser(self, username, email, password=None):
         """
         Creates and saves a staff user with a given email and password
         """
+        if not username:
+            raise ValueError("Users must have a valid username")
+        if not email:
+            raise ValueError("Users must have a valid email address")
+        user = self.model(
+            username=username,
+            email=email,
+            is_staff=True
+        )
+        user.set_passwod(password)
+        user.save(using=self._db)
+        return user
+        
 
-    def create_superuser(self, username, password=None):
+        
+    def create_superuser(self, username, email,  password=None):
         """
         Creates and saves a superuser with the given email and password
         """
         user = self.create_user(
             username=username,
+            email=email,
             is_staff=True,
             password=password
         )
