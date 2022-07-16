7d384298c77708cc33fbdce26837d15e9949824f
VincentGichohi <vincentegichohi@gmail.com>
2022-07-16 23:26:37 +0300

    Add a new field to the user model

--- scholarship_management/users/models.py
+++ scholarship_management/users/models.py
@@ -12,3 +12,19 @@ ALLOWED_GENDER = [
     ("MALE", "MALE"),
     ("FEMALE", "FEMALE")
 ]
+
+class BaseModel(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_deleted = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+class MyUser(BaseModel, AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255, blank=False, null=False)
+    last_name = models.CharField(max_length=255, blank=False, null=False)
+    address = models.CharField(max_length=255)
+    
