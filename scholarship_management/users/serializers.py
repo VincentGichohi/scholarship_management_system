ea2b3842e41c14478adae204a122eb4423ddc79f
VincentGichohi <vincentegichohi@gmail.com>
2022-07-17 23:59:38 +0300

    Add a registration serializer

--- scholarship_management/users/serializers.py
+++ scholarship_management/users/serializers.py
@@ -1,3 +1,5 @@
+from secrets import choice
+from tkinter import ALL
 from django.db.models import Q
 from rest_framework import serializers
 from users import models as user_models
@@ -9,4 +11,27 @@ ALLOWED_GENDER = [
 
 class BaseSerializer(serializers.Serializer):
     user_id = serializers.UUIDField(required=True)
-    
+
+class RegistrationSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    phone = serializers.CharField(required=True)
+    name = serializers.CharField(required=True)
+    gender = serializers.ChoiceField(required=True, choices=ALLOWED_GENDER)
+    password = serializers.CharField(required=True, trim_whitespace=True)
+    confirn_password = serializers.CharField(required=True, trim_whitespace=True)
+
+    def validate(self, obj):
+        password=obj['password']
+        password2=obj['password2']
+        email=obj['email']
+        phone=obj['phone']
+        qs = user_models.User.objects.filter(Q(username=email) | Q(phone=phone))
+
+        if qs.exists():
+            raise serializers.ValidationError("Users with email or phone number exists")
+        
+        if password != password2:
+            raise serializers.ValidationError("Password's do not match")
+
+        return obj
+        
