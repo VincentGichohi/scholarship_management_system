5d278a946beb606ed0a6821d70dda4cfbda077d5
VincentGichohi <vincentegichohi@gmail.com>
2022-07-17 23:49:59 +0300

    Add gender roles to the serializer

--- scholarship_management/users/serializers.py
+++ scholarship_management/users/serializers.py
@@ -2,3 +2,7 @@ from django.db.models import Q
 from rest_framework import serializers
 from users import models as user_models
 
+ALLOWED_GENDER = [
+    ("MALE", "MALE"),
+    ("FEMALE", "FEMALE")
+]
