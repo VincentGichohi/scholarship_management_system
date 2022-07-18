faa5bf9c884caec487443212217a17042ca82299
VincentGichohi <vincentegichohi@gmail.com>
2022-07-18 09:10:55 +0300

    Add a variable klass to views

--- scholarship_management/users/views.py
+++ scholarship_management/users/views.py
@@ -4,3 +4,6 @@ from users import serializers as user_serializers, models as user_models
 from django.contrib.auth import authenticate
 from oauth2_provider.models import get_application_model
 from users.utils import oauth2_users
+
+klass = oauth2_users.ApplicationUser()
+
