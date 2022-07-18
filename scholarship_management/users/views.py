a7f5e4de2cda8db87298a58758fe909de27370cd
VincentGichohi <vincentegichohi@gmail.com>
2022-07-18 09:00:53 +0300

    Add further imports to views

--- scholarship_management/users/views.py
+++ scholarship_management/users/views.py
@@ -1,2 +1,4 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from users import serializers as user_serializers, models as user_models
+from django.contrib.auth import authenticate
