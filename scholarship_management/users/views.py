eb7d9676e96616705085aca2571e20417f98bea5
VincentGichohi <vincentegichohi@gmail.com>
2022-07-18 09:08:30 +0300

    Add oauth2 authenticator import

--- scholarship_management/users/views.py
+++ scholarship_management/users/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from users import serializers as user_serializers, models as user_models
 from django.contrib.auth import authenticate
+from oauth2_provider.models import get_application_model
+from users.utils import oauth2_users
