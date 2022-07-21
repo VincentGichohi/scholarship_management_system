420c14c3ba352dc16d2d5a545c7553f174b61e74
VincentGichohi <vincentegichohi@gmail.com>
2022-07-21 10:28:52 +0300

    Modify the urls

--- scholarship_management/users/urls.py
+++ scholarship_management/users/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from users import views
+
+router = DefaultRouter(trailing_slash=False)
+
+router.register("accounts", views.authentication, basename="accounts")
+
+urlpatterns = [
+    path("registration", views.RegistationView.as_view()),
+]
